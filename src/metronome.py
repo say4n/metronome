b644e0ee3f6e7d3cd2df0a72d2be9663360a6c96
Sayan Goswami <goswami.sayan47@gmail.com>
2019-09-08 01:04:26 +0530

    add on_keypress handler
    adjust arguments interactively

--- src/metronome.py
+++ src/metronome.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import argparse
+import curses
 import os
 import subprocess
 import time
@@ -18,28 +19,71 @@ parser.add_argument("-c",
                     type=int,
                     default=4)
 
-args = parser.parse_args()
 
-pause = 60/args.bpm
-cycler = 0
+def on_keypress(key, win):
+    global args, step, terminate
 
-base_path = os.path.dirname(os.path.realpath(__file__))
-audio_files = [
-    os.path.join(base_path, "sounds/metronomeup.wav"),
-    os.path.join(base_path, "sounds/metronome.wav")
-]
+    if key == 'KEY_UP':
+        args.bpm = min(args.bpm + step, 240)
+        win.addstr(f"BPM: {args.bpm}")
+    elif key == 'KEY_DOWN':
+        args.bpm = max(args.bpm - step, 0)
+        win.addstr(f"BPM: {args.bpm}")
+    elif key == 'KEY_RIGHT':
+        step = min(step + 1, 10)
+        win.addstr(f"step: {step}")
+    elif key == 'KEY_LEFT':
+        step = max(step - 1, 1)
+        win.addstr(f"step: {step}")
+    elif key == 'q' or key == 'Q':
+        terminate = True
 
 
-while True:
-    if cycler % args.cycles == 0:
-        # play beep
-        subprocess.Popen(["ffplay", "-nodisp", "-autoexit", audio_files[0]],
-                         stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-    else:
-        # play bop
-        subprocess.Popen(["ffplay", "-nodisp", "-autoexit", audio_files[1]],
-                         stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+def main(win):
+    cycler = 0
+    key = None
+    win.nodelay(True)
 
-    cycler = (cycler + 1) % args.cycles
+    while True:
+        if terminate:
+            curses.endwin()
+            break
 
-    time.sleep(pause)
+        try:
+            key = win.getkey()
+            win.refresh()
+            on_keypress(key, win)
+        except Exception:
+            pass
+
+        if cycler % args.cycles == 0:
+            # play beep
+            subprocess.Popen(["ffplay", "-nodisp", "-autoexit",
+                              audio_files[0]],
+                             stdout=subprocess.DEVNULL,
+                             stderr=subprocess.STDOUT)
+        else:
+            # play bop
+            subprocess.Popen(["ffplay", "-nodisp", "-autoexit",
+                              audio_files[1]],
+                             stdout=subprocess.DEVNULL,
+                             stderr=subprocess.STDOUT)
+
+        cycler = (cycler + 1) % args.cycles
+
+        pause = 60/args.bpm
+        time.sleep(pause)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    step = 1
+    terminate = False
+
+    base_path = os.path.dirname(os.path.realpath(__file__))
+    audio_files = [
+        os.path.join(base_path, "sounds/metronomeup.wav"),
+        os.path.join(base_path, "sounds/metronome.wav")
+    ]
+
+    curses.wrapper(main)
